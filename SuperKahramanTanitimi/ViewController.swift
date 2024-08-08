9e3b096518af100b2633fada737e4fdd6663e799
Enisuzunn <enisbesiktasli@gmail.com>
2024-08-08 13:36:54 +0300

    Initial Commit

---
+++ SuperKahramanTanitimi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperKahramanTanitimi
+//
+//  Created by Enis Uzun on 8.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
